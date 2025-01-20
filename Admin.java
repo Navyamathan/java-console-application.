72ec4c178a16f84f7e77531e07bd75ad7ee6b771
Navyamathan <navyamathan@gmail.com>
2025-01-20 17:32:05 +0530

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,22 +1,4 @@
-public class Admin
-{
-    private String userName;
-    private String password;
-
-    public Admin(String userName, String password)
-    {
-        this.userName = userName;
-        this.password = password;
-    }
-
-    public String getUserName()
-    {
-        return userName;
-    }
-
-    public String getPassword()
-    {
-        return password;
-    }
-
-}
+git rm Admin.java
+    git commit _m "Deleted Admin.java"
+    git push
+    
