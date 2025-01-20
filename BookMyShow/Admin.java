fc69257461145fc6605a24be75ef9c36dda24f82
Navyamathan <navyamathan@gmail.com>
2025-01-20 17:19:35 +0530

    20/ 01/ 2025

---
+++ BookMyShow/Admin.java
@@ -0,0 +1,22 @@
+public class Admin
+{
+    private String userName;
+    private String password;
+
+    public Admin(String userName, String password)
+    {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+}
---
+++ BookMyShow/AdminActions.java
@@ -0,0 +1,302 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+class AdminActions
+{
+    Utilities u = new Utilities();
+
+    public Admin adminLogin(Scanner s)
+    {
+        int attempts = 0;
+        System.out.println("WELCOME TO ADMIN LOGIN SYSTEM...");
+
+        for (Admin tempAccount : BookMyShow.getAdminArrayList())
+        {
+            while (attempts < 3)
+            {
+                System.out.println();
+                System.out.print("Enter User Name: ");
+                String username = s.nextLine();
+                if (username.equals(tempAccount.getUserName()))
+                {
+                    System.out.print("Enter Password: ");
+                    String password = s.nextLine();
+                    if (password.equals(tempAccount.getPassword()))
+                    {
+                        return tempAccount;
+                    } else {
+                        attempts++;
+                        System.out.println();
+                        System.out.println("Wrong Password!");
+                        if (attempts == 3)
+                        {
+                            Admin ob = new Admin(null, null);
+                            return ob;
+                        }
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+        return null;
+    }
+
+    public void adminAction(Scanner s)
+    {
+        System.out.println();
+        System.out.println("YOU ENTER THE ADMIN BOARD...");
+        while (true)
+        {
+            System.out.println();
+            System.out.println("1. ADD THEATRE \n2. VIEW THEATRE DETAILS \n3. ADD MOVIE \n4. VIEW MOVIE \n5. EXIT");
+            System.out.print("Enter Your Choice: ");
+            int choice = s.nextInt();
+            s.nextLine();
+            System.out.println();
+            if (choice == 1)
+            {
+                addTheatre(s);
+            }
+            else if(choice == 2)
+            {
+                viewTheatreDetails();
+            }
+            else if (choice == 3)
+            {
+                addMovie(s);
+            }
+            else if (choice == 4)
+            {
+                viewMovie();
+            }
+            else if (choice == 5)
+            {
+                break;
+            }
+            else
+            {
+                System.out.println("Enter The Valid Choice!");
+            }
+        }
+    }
+
+    public void addTheatre(Scanner s)
+    {
+        System.out.println("To Add Theatres Enter The Following Details...");
+        System.out.println();
+        System.out.print("Enter Theater Name: ");
+        String theatreName = s.nextLine();
+        System.out.print("Enter Location Of The Theater: ");
+        String theatreLocation = s.nextLine();
+        System.out.print("Enter Theater Screen Count: ");
+        int screenCount = s.nextInt();
+        s.nextLine();
+        Theatre t = new Theatre(theatreName, theatreLocation, screenCount);
+        BookMyShow.getTheatreArrayList().add(t);
+        for (int i = 1; i <= screenCount; i++)
+        {
+            System.out.println();
+            System.out.println("Enter Details About Screen Number " + i + "...");
+            System.out.print("Enter Screen Name: ");
+            String screenName = s.nextLine();
+            System.out.print("Enter The Number Of Seats: ");
+            int seatsCount = s.nextInt();
+            s.nextLine();
+            String seatsGrid;
+            while(true)
+            {
+                System.out.print("Enter Seats Grid: ");
+                seatsGrid = s.nextLine();
+                HashMap<Character,ArrayList<String>> seats = u.calculateGrid(seatsCount,seatsGrid);
+                if(seats!=null)
+                {
+                    t.getScreenArrayList().add(new Screen(screenName,seatsCount,seats));
+                    break;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("Theatre Added Successfully!");
+    }
+
+    public void viewTheatreDetails()
+    {
+        System.out.println("...Details Of Theatre...");
+        System.out.println();
+        for(Theatre tempTheatre : BookMyShow.getTheatreArrayList())
+        {
+            System.out.println("Theatre Name: "+tempTheatre.getTheatreName());
+            System.out.println("Theatre Location: "+tempTheatre.getTheatreLocation());
+            System.out.println("Screen Count :"+tempTheatre.getScreenCount());
+            int i=1;
+            for(Screen tempScreen : tempTheatre.getScreenArrayList())
+            {
+                System.out.println();
+                System.out.println("Details of Screen "+i+"...");
+                System.out.println("Screen Name: "+tempScreen.getScreenName());
+                System.out.println("Seats Count: "+tempScreen.getSeatsCount());
+                System.out.println("Seats Arrangements: ");
+                u.printSeats(tempScreen.getSeatsGrid());
+                i++;
+            }
+        }
+    }
+
+    public void addMovie(Scanner s)
+    {
+        Screen screen = null;
+        Theatre theatre = null;
+        Show show;
+        String location;
+        LocalDate localDate;
+        String theatreName;
+        String screenName;
+        boolean theatreFound = false;
+        System.out.println("Enter the Following Details to Add Movie...");
+        System.out.println();
+        System.out.print("Enter The Movie Name: ");
+        String movieName = s.nextLine();
+        System.out.print("Enter The Date( dd_MM_yyyy ): ");
+        String date = s.nextLine();
+        localDate = LocalDate.parse(date,Theatre.getLocalDateFormatter());
+        System.out.print("Enter The Duration( HH:mm ): ");
+        String duration = s.nextLine();
+        LocalTime localTime = LocalTime.parse(duration,Theatre.getLocalTimeFormatter());
+        System.out.print("Enter the Prize: ");
+        long price = s.nextLong();
+        s.nextLine();
+
+        while(true)
+        {
+            System.out.print("Enter The Location: ");
+            location = s.nextLine();
+
+            System.out.println("Available Theatres: ");
+            for(Theatre tempTheatre : BookMyShow.getTheatreArrayList())
+            {
+                if(tempTheatre.getTheatreLocation().equals(location))
+                {
+                    System.out.println("* "+tempTheatre.getTheatreName());
+                    theatreFound = true;
+                }
+            }
+            if(theatreFound)
+            {
+                break;
+            }
+            else
+            {
+                System.out.println("There Is No Theatre In The Location You Select");
+            }
+        }
+
+        while(true)
+        {
+            System.out.print("Enter The Name Of The Theatre You Want To Add: ");
+            theatreName = s.nextLine();
+
+            System.out.println("Available Screens: ");
+            for(Theatre tempTheatre : BookMyShow.getTheatreArrayList())
+            {
+                if (tempTheatre.getTheatreName().equals(theatreName))
+                {
+                    theatre = tempTheatre;
+                    for (Screen tempScreen : tempTheatre.getScreenArrayList())
+                    {
+                        System.out.println("* "+tempScreen.getScreenName());
+                    }
+                }
+            }
+            if(theatre != null)
+            {
+                break;
+            }
+            else
+            {
+                System.out.println("Theatre Not Found!");
+            }
+        }
+
+        while(true)
+        {
+            System.out.print("Enter The Name Of The Screen You Want To Add: ");
+            screenName = s.nextLine();
+
+            for(Screen tempScreen : theatre.getScreenArrayList())
+            {
+                if (screenName.equals(tempScreen.getScreenName()))
+                {
+                    screen = tempScreen;
+                }
+            }
+            if(screen != null)
+            {
+                break;
+            }
+            else
+            {
+                System.out.println("Screen Not Found!");
+            }
+        }
+
+        while(true)
+        {
+            boolean flag = false;
+            System.out.print("Enter The Show Starting Time( HH:mm ): ");
+            String startingTime = s.nextLine();
+            LocalTime localStartingTime = LocalTime.parse(startingTime,Theatre.getLocalTimeFormatter());
+            LocalTime localEndingTime = localStartingTime.plusHours(localTime.getHour()).plusMinutes(localTime.getMinute()).plusMinutes(30);
+            for(Movie tempMovie : Theatre.getMovieArrayList())
+            {
+                if ((tempMovie.getDate().equals(localDate)) && (tempMovie.getTheatre().getTheatreName().equals(theatreName)) && (tempMovie.getScreen().getScreenName().equals(screenName)))
+                {
+                    if (!(localEndingTime.isBefore(tempMovie.getShow().getStartingTime()) || localStartingTime.isAfter(tempMovie.getShow().getEndingTime())))
+                    {
+                        flag = true;
+                    }
+                }
+            }
+            if(flag)
+            {
+                System.out.println("Show Is Already Exist In Given Time!");
+            }
+            else
+            {
+                show = new Show(localStartingTime, localEndingTime);
+                Theatre.getShowArrayList().add(show);
+                System.out.println("Movie Added Successfully!");
+                break;
+            }
+        }
+        Theatre.getMovieArrayList().add(new Movie(movieName,localDate,localTime,price,location,theatre,screen,show));
+    }
+
+    public void viewMovie()
+    {
+        System.out.println("...The Details Of Movies...");
+        for(Movie tempMovie : Theatre.getMovieArrayList())
+        {
+            System.out.println();
+            System.out.println("Movie Name: "+tempMovie.getMovieName());
+            System.out.println("Date: "+tempMovie.getDate());
+            System.out.println("Duration: "+tempMovie.getDuration());
+            System.out.println("Price: "+tempMovie.getPrice());
+            System.out.println("Location: "+tempMovie.getLocation());
+            System.out.println("Theatre: "+tempMovie.getTheatre().getTheatreName());
+            System.out.println("Screen: "+tempMovie.getScreen().getScreenName());
+            System.out.println("Show Starting Time: "+tempMovie.getShow().getStartingTime());
+            System.out.println("Show Ending Time: "+tempMovie.getShow().getEndingTime());
+        }
+    }
+
+}
+
+
+
+
