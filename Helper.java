e25a39bd8496d1d10ec7a73a6220abe0dc705979
Navyamathan <navyamathan@gmail.com>
2025-01-20 17:50:18 +0530

    Delete Helper.java

--- Helper.java
+++
@@ -1,8 +0,0 @@
-public class Helper
-{
-    public static void main(String[] args)
-    {
-        BookMyShowActions bookMyShowActions = new BookMyShowActions();
-        bookMyShowActions.start();
-    }
-}
